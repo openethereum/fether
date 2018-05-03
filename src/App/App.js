8de8b39227e9f0a7c597be6015149c46f4336e63
Amaury Martiny <amaury.martiny@gmail.com>
2018-05-03 14:50:55 +0200

    Change header of files

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,7 @@
 // Copyright 2015-2018 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
-
+//
 // SPDX-License-Identifier: MIT
-
-/* eslint-disable jsx-a11y/aria-role */
 
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
