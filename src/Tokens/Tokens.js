0f92c6f45ea445184de55fb15c678ac325061c40
Amaury Martiny <amaury.martiny@gmail.com>
2018-05-04 09:17:53 +0200

    Add node health and sync explanations

--- src/Tokens/Tokens.js
+++ src/Tokens/Tokens.js
@@ -5,7 +5,7 @@
 
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { defaultAccount$ } from '@parity/light.js';
+import { defaultAccount$, nodeHealth$ } from '@parity/light.js';
 
 import EthBalance from './EthBalance';
 import light from '../hoc';
@@ -14,31 +14,58 @@ import TokenBalance from './TokenBalance';
 @inject('tokensStore')
 @observer
 @light({
-  me: defaultAccount$
+  me: defaultAccount$,
+  nodeHealth: nodeHealth$
 })
 class Tokens extends Component {
-  render () {
+  render() {
     const {
       me,
+      nodeHealth,
       tokensStore: { tokens }
     } = this.props;
 
-    if (!me) {
-      return null;
-    }
-
     return (
-      <div className='box -scroller'>
-        <ul className='list -tokens'>
-          {Array.from(tokens.keys()).map(key => (
-            <li key={key}>
-              {key === 'ETH' ? (
-                <EthBalance address={me} token={key} {...tokens.get(key)} />
-              ) : (
-                <TokenBalance address={me} token={key} {...tokens.get(key)} />
-              )}
+      <div className="box -scroller">
+        <ul className="list -tokens">
+          {me &&
+            Array.from(tokens.keys()).map(key => (
+              <li key={key}>
+                {key === 'ETH' ? (
+                  <EthBalance address={me} token={key} {...tokens.get(key)} />
+                ) : (
+                  <TokenBalance address={me} token={key} {...tokens.get(key)} />
+                )}
+              </li>
+            ))}
+          {nodeHealth && (
+            <li>
+              <p>2. Overall node health status</p>
+              <pre>
+                PEERS: {nodeHealth.peers.status} {nodeHealth.peers.details[0]}/{
+                  nodeHealth.peers.details[1]
+                }
+                <br />SYNC: {nodeHealth.sync.status}
+                <br />TIMESYNC: {nodeHealth.time.status}
+              </pre>
+              <p>
+                Note: I can make a small algorithm which outputs the average
+                health with 3 states: OK, ALRIGHT, and BAD
+              </p>
             </li>
-          ))}
+          )}
+          {nodeHealth && (
+            <li>
+              <p>
+                3. When SYNC above is false, we have the syncing progress to
+                give an idea how much time it'll take
+              </p>
+              <pre>
+                "startingBlock": 900<br />"currentBlock": 902<br />
+                "highestBlock": 1108
+              </pre>
+            </li>
+          )}
         </ul>
       </div>
     );
