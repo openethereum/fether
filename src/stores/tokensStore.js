b40cf790d0b53993f4948bc19a9fc84c4f588e29
Amaury Martiny <amaury.martiny@protonmail.com>
2018-05-18 12:39:12 +0200

    One LS key per chain for tokens

--- src/stores/tokensStore.js
+++ src/stores/tokensStore.js
@@ -7,26 +7,27 @@ import { action, computed, observable } from 'mobx';
 import store from 'store';
 
 import ethereumIcon from '../assets/img/tokens/ethereum.png';
+import parityStore from './parityStore';
 
-const LS_PREFIX = '__paritylight::';
-const LS_KEY = `${LS_PREFIX}tokens`;
+const LS_PREFIX = '__paritylight::tokens';
 
 class TokensStore {
   @observable tokens = new Map();
 
   constructor () {
-    const value = store.get(LS_KEY);
+    this.api = parityStore.api;
 
-    if (!value) {
-      // We consider Ethereum as a token, with address 'ETH'
-      this.addToken('ETH', {
-        address: 'ETH',
-        logo: ethereumIcon,
-        name: 'Ethereum',
-        symbol: 'ETH'
-      });
+    if (parityStore.isApiConnected) {
+      this.init();
     } else {
-      this.tokens.replace(value);
+      // TODO This .on() is not working, so we poll every second
+      // this.api.on('connected', this.init);
+      this.interval = setInterval(() => {
+        if (parityStore.isApiConnected) {
+          this.init();
+          clearInterval(this.interval);
+        }
+      }, 1000);
     }
   }
 
@@ -34,6 +35,34 @@ class TokensStore {
   addToken = (address, token) => {
     this.tokens.set(address, token);
     this.updateLS();
+  };
+
+  init = async () => {
+    // Set the localStorage key, we have one key per chain, in this format:
+    // __paritylight::tokens::kovan
+    try {
+      const chainName = await this.api.parity.chain();
+      this.lsKey = `${LS_PREFIX}::${chainName}`;
+    } catch (e) {
+      console.error(e);
+      // Fallback to main net
+      this.lsKey = `${LS_PREFIX}::foundation`;
+    }
+
+    // Now we fetch the tokens from the localStorage
+    const tokens = store.get(this.lsKey);
+    if (!tokens) {
+      // If there's nothing in the localStorage, we add be default only
+      // Ethereum. We consider Ethereum as a token, with address 'ETH'
+      this.addToken('ETH', {
+        address: 'ETH',
+        logo: ethereumIcon,
+        name: 'Ethereum',
+        symbol: 'ETH'
+      });
+    } else {
+      this.tokens.replace(tokens);
+    }
   };
 
   @action
@@ -54,7 +83,12 @@ class TokensStore {
     );
   }
 
-  updateLS = () => store.set(LS_KEY, this.tokens);
+  updateLS = () => {
+    if (!this.lsKey) {
+      return;
+    }
+    store.set(this.lsKey, this.tokens);
+  };
 }
 
 export default new TokensStore();
