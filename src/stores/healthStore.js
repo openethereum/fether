92e31f6091ae888dff04a8af52bae2c1fa7e895d
Amaury Martiny <amaury.martiny@protonmail.com>
2018-06-05 16:47:56 +0200

    Fix clock not sync error

--- src/stores/healthStore.js
+++ src/stores/healthStore.js
@@ -25,7 +25,7 @@ class HealthStore {
   @observable nodeHealth;
   @observable syncing;
 
-  constructor () {
+  constructor() {
     nodeHealth$().subscribe(this.setNodeHealth);
     syncing$().subscribe(this.setSyncing);
   }
@@ -37,7 +37,7 @@ class HealthStore {
    * represents the current status, with a custom payload.
    */
   @computed
-  get health () {
+  get health() {
     // Check download progress
     if (parityStore.downloadProgress > 0 && !parityStore.isParityRunning) {
       return {
@@ -70,7 +70,7 @@ class HealthStore {
     if (this.syncing) {
       const { currentBlock, highestBlock, startingBlock } = this.syncing;
       const percentage = Math.round(
-        (currentBlock - startingBlock) * 100 / (highestBlock - startingBlock)
+        ((currentBlock - startingBlock) * 100) / (highestBlock - startingBlock)
       );
       return {
         status: STATUS.SYNCING,
@@ -129,7 +129,7 @@ class HealthStore {
 
     if (
       message.includes(
-        'Your clock is not in sync. Detected difference is too big for the protocol to work.'
+        'Your clock is not in sync. Detected difference is too big for the protocol to work'
       )
     ) {
       return { status: STATUS.CLOCKNOTSYNC, payload: message };
