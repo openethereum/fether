2fde91cf3c6022d269e2d89fdc79019363112162
Brian Flanagan <brian@elbow.ie>
2018-06-07 14:19:12 +0100

    beginning of some design for the error-state overlays

--- src/Overlay/NoInternet/NoInternet.js
+++ src/Overlay/NoInternet/NoInternet.js
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: MIT
 
 import React, { PureComponent } from 'react';
+import alert from '../../assets/img/icons/loading.svg';
 
 class NoInternet extends PureComponent {
   render () {
@@ -12,7 +13,13 @@ class NoInternet extends PureComponent {
         <div className='box -padded'>
           <div className='alert-screen'>
             <div className='alert-screen_content'>
-              Poor connection detected. Please check your network.
+              <div className='alert-screen_image'>
+                <img src={alert} />
+              </div>
+              <div className='alert-screen_text'>
+                <h1>Poor connection</h1>
+                <p>Please check that your network.</p>
+              </div>
             </div>
           </div>
         </div>
