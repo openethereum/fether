4f5646e1f3edc97a3adc292e15284ecc8b766058
Amaury Martiny <amaury.martiny@protonmail.com>
2018-06-08 14:40:04 +0200

    Fix bug script

--- packages/light-electron/scripts/fixElectronBug.sh
+++ packages/light-electron/scripts/fixElectronBug.sh
@@ -4,4 +4,7 @@
 # Apply this patch: https://github.com/electron-userland/electron-webpack/pull/154
 find='process.resourcesPath + "\/static"'
 replace='"${path.join(configurator.projectDir, "static").replace(\/\\\\\/g, "\\\\\\\\")}"'
-sed -i '.bak' "s/$find/$replace/g" ./node_modules/electron-webpack/out/targets/MainTarget.js
+case "$OSTYPE" in
+  darwin*)  sed -i '.bak' "s/$find/$replace/g" ./node_modules/electron-webpack/out/targets/MainTarget.js ;; 
+  *)        sed -i "s/$find/$replace/g" ./node_modules/electron-webpack/out/targets/MainTarget.js ;;
+esac
