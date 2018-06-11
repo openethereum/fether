69c3846936ada20a7f1062480764b881a3fde1ab
Amaury Martiny <amaury.martiny@protonmail.com>
2018-06-11 15:08:27 +0200

    Use JS for babel config

---
+++ packages/light-hoc/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    ['@babel/preset-stage-0', { decoratorsLegacy: true }]
+  ]
+};
