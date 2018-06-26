9f6b6d53edaa8de12ade071628891135f59f9af8
Amaury Martiny <amaury.martiny@protonmail.com>
2018-06-26 17:05:39 +0200

    Add open logs button on error

--- packages/fether-electron/src/main/operations/handleError.js
+++ packages/fether-electron/src/main/operations/handleError.js
@@ -3,18 +3,20 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-import { app, dialog } from 'electron';
+import { app, dialog, shell } from 'electron';
 
 import { bugs, name, parity } from '../../../package.json';
 import Pino from '../utils/pino';
 
 const pino = Pino();
+const logFile = `${app.getPath('userData')}/${name}.log`;
 
 export default (err, message = 'An error occurred.') => {
   pino.error(err);
   dialog.showMessageBox(
     {
-      buttons: ['OK'],
+      buttons: ['OK', 'Open logs'],
+      defaultId: 0,
       detail: `Please attach the following debugging info:
 OS: ${process.platform}
 Arch: ${process.arch}
@@ -22,11 +24,19 @@ Channel: ${parity.channel}
 Error: ${err.message}
 
 Please also attach the contents of the following file:
-${app.getPath('userData')}/${name}.log`,
+${logFile}`,
       message: `${message} Please file an issue at ${bugs.url}.`,
       title: 'Parity Error',
       type: 'error'
     },
-    () => app.exit(1)
+    buttonId => {
+      switch (buttonId) {
+        case 1:
+          shell.openItem(logFile);
+          break;
+        default:
+          app.exit(1);
+      }
+    }
   );
 };
