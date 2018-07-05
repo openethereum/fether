9c9f34a0bde7eb53271331a2d6ccc12a5c25b5a8
Amaury Martiny <amaury.martiny@protonmail.com>
2018-07-05 13:50:16 +0200

    Remove react-tooltip

--- packages/fether-ui/src/ClickToCopy/ClickToCopy.js
+++ packages/fether-ui/src/ClickToCopy/ClickToCopy.js
@@ -4,8 +4,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import React, { PureComponent } from 'react';
+import { Popup } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
+import 'semantic-ui-css/components/popup.min.css';
 
 export class ClickToCopy extends PureComponent {
   static defaultProps = {
@@ -21,10 +22,6 @@ export class ClickToCopy extends PureComponent {
     copied: false
   };
 
-  componentWillUnmount () {
-    window.clearTimeout(this.timeout);
-  }
-
   handleCopy = () => {
     // https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
     // Note react-copy-to-clipboard created a bug, https://github.com/nkbt/react-copy-to-clipboard/issues/92
@@ -36,35 +33,27 @@ export class ClickToCopy extends PureComponent {
     document.body.removeChild(el);
 
     this.setState({ copied: true });
-
-    // Show the copied state only for 1s
-    if (this.timeout) {
-      window.clearTimeout(this.timeout);
-    }
-    this.timeout = window.setTimeout(
-      () => this.setState({ copied: false }),
-      1000
-    );
   };
 
-  handleResetCopied = () => {};
+  handleResetCopied = () => this.setState({ copied: false });
 
   render () {
     const { children, label, ...otherProps } = this.props;
     const { copied } = this.state;
 
     return (
-      <div data-tip='' onClick={this.handleCopy}>
-        {children}
-        <ReactTooltip
-          effect='solid'
-          event='mouseover'
-          eventOff='mouseout'
-          place='bottom'
-          getContent={[() => (copied ? 'Copied.' : label), 50]}
-          {...otherProps}
-        />
-      </div>
+      <Popup
+        content={copied ? 'Copied.' : 'Click to copy'}
+        inverted
+        onClose={this.handleResetCopied}
+        position='bottom center'
+        size='mini'
+        trigger={
+          <div onClick={this.handleCopy} {...otherProps}>
+            {children}
+          </div>
+        }
+      />
     );
   }
 }
