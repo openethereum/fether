09174275ddea196ec65c897eaa8222bbc0ecae55
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-07 21:42:53 +0200

    Use remote @parity/electron

--- packages/parity-electron/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2015-2018 Parity Technologies (UK) Ltd.
-// This file is part of Parity.
-//
-// SPDX-License-Identifier: BSD-3-Clause
-
-export type LoggerFunction = (namespace: string) => (log: string) => void;
