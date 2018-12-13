35262e5f9078dffa0d7867eb43af021d48dbc0a1
Luke Schoen <ltfschoen@gmail.com>
2018-12-13 13:01:27 +0100

    refactor: Simplify since macOS Sed installed by default so do not need check

--- packages/fether-electron/scripts/fixElectronBug.sh
+++ packages/fether-electron/scripts/fixElectronBug.sh
@@ -3,30 +3,11 @@
 # There is a bug with __static in Electron when running `yarn electron`
 # Apply this patch: https://github.com/electron-userland/electron-webpack/pull/154
 # It's basically a find/replace in a file
-PWD="$(pwd)"
-# Note: This may return `/usr/local/bin/sed /usr/bin/sed` if both macOS Sed and GNU Sed are installed.
-SED_MAC_DIR="$(which -a sed)"
 FIND='process.resourcesPath + "\/static"'
 REPLACE='"${path.join(configurator.projectDir, "static").replace(\/\\\\\/g, "\\\\\\\\")}"'
-# Note: This must be run from the Fether project root directory
-FILE="${PWD}/node_modules/electron-webpack/out/targets/MainTarget.js"
+FILE="./node_modules/electron-webpack/out/targets/MainTarget.js"
 case "$OSTYPE" in
-  darwin*)
-    # Run explicit macOS in-built Sed command (i.e. `/usr/bin/sed` instead of just `sed`) since it is supported.
-    # Note: On macOS it is necessary to run the Sed command explicitly because the `sed` command may not be in the 
-    # users PATH, or the `sed` binary may actually have been configured to run GNU Sed (not supported) instead.
-    # of the macOS in-built Sed (supported).
-    #
-    # For example, instead of a user installing GNU Sed with `brew install gnu-sed` (which allows GNU Sed
-    # binary to be run with `/usr/local/bin/gsed`), they may have installed it with 
-    # `brew install --with-default-names gnu-sed` (which allows the GNU Sed binary to be run with `sed` or 
-    # `/usr/local/bin/sed`), so running just `sed` would run GNU Sed instead of macOS in-built Sed.
-    if [[ $SED_MAC_DIR == *"/usr/bin/sed"* ]] ;
-    then
-      /usr/bin/sed -i '.bak' "s/$FIND/$REPLACE/g" $FILE
-    else
-      echo 'Error: macOS Sed not found'
-    fi
-    ;;
-  *) sed -i "s/$FIND/$REPLACE/g" $FILE
+  # On macOS (darwin), run the in-built Sed binary explicitly to avoid any conflicts with GNU Sed
+  darwin*) /usr/bin/sed -i '.bak' "s/$FIND/$REPLACE/g" $FILE ;;
+  *)       sed -i "s/$FIND/$REPLACE/g" $FILE ;;
 esac
--- packages/fether-electron/scripts/revertElectronBug.sh
+++ packages/fether-electron/scripts/revertElectronBug.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
 # This is the revert patch from ./fixElectronBug.sh
-PWD="$(pwd)"
-# Note: This may return `/usr/local/bin/sed /usr/bin/sed` if both macOS Sed and GNU Sed are installed.
-SED_MAC_DIR="$(which -a sed)"
 FIND='"${path.join(configurator.projectDir, "static").replace(\/\\\\\/g, "\\\\\\\\")}"'
 REPLACE='process.resourcesPath + "\/static"'
-# Note: This must be run from the Fether project root directory
-FILE="${PWD}/node_modules/electron-webpack/out/targets/MainTarget.js"
+FILE="./node_modules/electron-webpack/out/targets/MainTarget.js"
 case "$OSTYPE" in
-  darwin*)
-    # Refer to comments in ./fixElectronBug.sh
-    if [[ $SED_MAC_DIR == *"/usr/bin/sed"* ]] ;
-    then
-      /usr/bin/sed -i '.bak' "s/$FIND/$REPLACE/g" $FILE
-    else
-      echo 'Error: macOS Sed not found'
-    fi
-    ;;
-  *) sed -i "s/$FIND/$REPLACE/g" $FILE
+  darwin*) /usr/bin/sed -i '.bak' "s/$FIND/$REPLACE/g" $FILE ;;
+  *)       sed -i "s/$FIND/$REPLACE/g" $FILE ;;
 esac
