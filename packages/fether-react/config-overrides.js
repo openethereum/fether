8b7d9edd6fc9263496e00d9ce2ea330250003602
Axel Chalon <xaxel@protonmail.com>
2018-12-14 17:02:46 +0100

    Update fether-react to create-react-app 2 (closes #212)

--- packages/fether-react/config-overrides.js
+++ packages/fether-react/config-overrides.js
@@ -3,7 +3,9 @@ const { injectBabelPlugin } = require('react-app-rewired');
 /* config-overrides.js */
 module.exports = function override (config) {
   // use the MobX rewire to use @decorators
-  config = injectBabelPlugin('transform-decorators-legacy', config);
-
+  config = injectBabelPlugin(
+    ['@babel/proposal-decorators', { legacy: true }],
+    config
+  );
   return config;
 };
