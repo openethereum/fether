0509cee1577ea0938ba5bdd2afeee932ab7d354e
YJ <yjkimjunior@gmail.com>
2018-12-25 17:48:21 +0800

    WIP: refactor Field component as class

--- packages/fether-ui/src/Form/Field/Field.js
+++ packages/fether-ui/src/Form/Field/Field.js
@@ -6,31 +6,48 @@
 import React from 'react';
 import { Popup } from 'semantic-ui-react';
 import 'semantic-ui-css/components/popup.min.css';
+import PropTypes from 'prop-types';
 
-export const Field = ({
-  as: T = 'input',
-  children,
-  input,
-  label,
-  meta,
-  ...otherProps
-}) => (
-  <div className='form_field'>
-    <label htmlFor={input && input.name}>{label}</label>
-    <Popup
-      content={meta && (meta.error || meta.submitError)}
-      inverted
-      on='click'
-      open={
-        !!meta &&
-        !meta.valid &&
-        (!meta.pristine || meta.touched) &&
-        !meta.dirtySinceLastSubmit
-      }
-      position='bottom center'
-      size='mini'
-      trigger={<T id={input && input.name} {...input} {...otherProps} />}
-    />
-    {children}
-  </div>
-);
+export class Field extends React.Component {
+  static propTypes = {
+    as: PropTypes.any,
+    children: PropTypes.node,
+    input: PropTypes.any,
+    label: PropTypes.string,
+    meta: PropTypes.object
+  };
+
+  static defaultProps = {
+    as: 'input'
+  };
+
+  render () {
+    const { as, children, input, label, meta, ...otherProps } = this.props;
+
+    const trigger = React.createElement(as, { id: input && input.name }, [
+      ...input,
+      ...otherProps
+    ]);
+
+    return (
+      <div className='form_field'>
+        <label htmlFor={input && input.name}>{label}</label>
+        <Popup
+          content={meta && (meta.error || meta.submitError)}
+          inverted
+          on='click'
+          open={
+            !!meta &&
+            !meta.valid &&
+            (!meta.pristine || meta.touched) &&
+            !meta.dirtySinceLastSubmit
+          }
+          position='bottom center'
+          size='mini'
+          trigger={trigger}
+        />
+        {children}
+      </div>
+    );
+  }
+}
