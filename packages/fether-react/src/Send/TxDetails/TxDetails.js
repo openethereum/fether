96b71099d62b4c38cf9f87b3b2ce0dc241a1b9f5
Luke Schoen <ltfschoen@gmail.com>
2019-01-03 00:19:37 +1100

    review-fix: Use fromWei to convert to ether values

--- packages/fether-react/src/Send/TxDetails/TxDetails.js
+++ packages/fether-react/src/Send/TxDetails/TxDetails.js
@@ -5,7 +5,7 @@
 
 import React, { Component } from 'react';
 import BigNumber from 'bignumber.js';
-import { toWei } from '@parity/api/lib/util/wei';
+import { fromWei, toWei } from '@parity/api/lib/util/wei';
 
 class TxDetails extends Component {
   state = {
@@ -34,8 +34,7 @@ class TxDetails extends Component {
   renderFee = values => {
     const { estimatedTxFee } = this.props;
 
-    return `Fee: ${estimatedTxFee(values)
-      .div(10 ** 18)
+    return `Fee: ${fromWei(estimatedTxFee(values), 'ether')
       .toFixed(9)
       .toString()} ETH (estimate * gas price)`;
   };
@@ -43,9 +42,12 @@ class TxDetails extends Component {
   renderTotalAmount = values => {
     const { estimatedTxFee, token } = this.props;
 
-    return `Total Amount: ${estimatedTxFee(values)
-      .plus(token.address === 'ETH' ? toWei(values.amount.toString()) : 0)
-      .div(10 ** 18)
+    return `Total Amount: ${fromWei(
+      estimatedTxFee(values).plus(
+        token.address === 'ETH' ? toWei(values.amount.toString()) : 0
+      ),
+      'ether'
+    )
       .toFixed(10)
       .toString()} ETH`;
   };
