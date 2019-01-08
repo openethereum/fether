34e52b11334f52f461ac53cd061d5d6a7b504491
Luke Schoen <ltfschoen@gmail.com>
2019-01-08 14:09:56 +1100

    refactor: Add newlines so steps are readable

--- packages/fether-electron/src/main/app/index.js
+++ packages/fether-electron/src/main/app/index.js
@@ -41,10 +41,13 @@ class FetherApp {
     if (options.withTaskbar) {
       this.createWindow();
       this.fetherApp.window.setProgressBar(0.4);
+
       this.loadTaskbar();
       this.fetherApp.window.setProgressBar(0.6);
+
       this.finalise();
       this.fetherApp.window.setProgressBar(0.8);
+
       this.showWindow();
       this.fetherApp.window.setProgressBar(1.0);
     } else {
@@ -54,12 +57,12 @@ class FetherApp {
       // Opens file:///path/to/build/index.html in prod mode, or whatever is
       // passed to ELECTRON_START_URL
       this.fetherApp.window.loadURL(options.index);
+
       this.finalise();
       this.fetherApp.window.setProgressBar(1.0);
     }
 
     this.fetherApp.window.setProgressBar(-1);
-
     this.fetherApp.emit('after-create-app');
   };
 
