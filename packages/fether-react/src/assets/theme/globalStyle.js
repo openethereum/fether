db903c8941dcd5a354d604b5c582146e87895f9b
Luke Schoen <ltfschoen@gmail.com>
2019-01-13 22:49:26 +1100

    refactor: Remove disused theme colours from fether-ui and integrate into fetherTheme

--- packages/fether-react/src/assets/theme/globalStyle.js
+++ packages/fether-react/src/assets/theme/globalStyle.js
@@ -11,10 +11,18 @@ const GlobalStyle = createGlobalStyle`
 
 const fetherTheme = {
   black: '#222',
+  blue: '#160DF6',
   chrome: '#f5f6f6',
   darkGrey: '#444444',
+  eggshell: '#fff',
   faint: '#ddd',
-  mono: 'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace'
+  green: '#3ec28f',
+  grey: '#888',
+  mono: 'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace',
+  purple: '#330044',
+  red: '#C2473E',
+  white: '#f9f9f9',
+  yellow: '#DFCA29'
 };
 
 export { fetherTheme, GlobalStyle };
