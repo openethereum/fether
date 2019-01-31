7c9eb5f9d703bbe1184b4570eadcc83b5f139bdc
Luke Schoen <ltfschoen@gmail.com>
2019-02-01 10:36:07 +1100

    test: Setup Jest

--- packages/fether-react/babel.config.js
+++ packages/fether-react/babel.config.js
@@ -3,5 +3,9 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-modules-commonjs'
   ],
-  presets: ['@babel/preset-env']
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    ['@babel/preset-stage-0', { decoratorsLegacy: true }]
+  ]
 };
