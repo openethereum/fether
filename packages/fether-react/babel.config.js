22ed464d5565df15da8144f9db323f74face0add
Luke Schoen <ltfschoen@gmail.com>
2019-02-01 10:45:21 +1100

    test: Fix pipelineOperator error that was failing tests
    Fixes error: 'pipelineOperator' requires 'proposal' option whose value should be one of: 'minimal', 'smart'

--- packages/fether-react/babel.config.js
+++ packages/fether-react/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
     '@babel/plugin-transform-modules-commonjs'
   ],
   presets: [
