2213a8fa4b1e8dd7c5b5dc51f63f316728a53434
Luke Schoen <ltfschoen@gmail.com>
2019-02-05 02:06:14 +1100

    chore: Refactor using Babel plugin syntax from guide
    In the Babel 7 migration guide these are the different plugings and
    to add them:
    
    See https://babeljs.io/blog/2018/07/27/removing-babels-stage-presets#migrating
    
    ```
    // Stage 0
    "@babel/plugin-proposal-function-bind",
    
    // Stage 1
    "@babel/plugin-proposal-export-default-from",
    "@babel/plugin-proposal-logical-assignment-operators",
    ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
    ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
    ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
    "@babel/plugin-proposal-do-expressions",
    
    // Stage 2
    ["@babel/plugin-proposal-decorators", { "legacy": true }],
    "@babel/plugin-proposal-function-sent",
    "@babel/plugin-proposal-export-namespace-from",
    "@babel/plugin-proposal-numeric-separator",
    "@babel/plugin-proposal-throw-expressions",
    
    // Stage 3
    "@babel/plugin-syntax-dynamic-import",
    "@babel/plugin-syntax-import-meta",
    ["@babel/plugin-proposal-class-properties", { "loose": false }],
    "@babel/plugin-proposal-json-strings"
    ```

--- packages/fether-ui/babel.config.js
+++ packages/fether-ui/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
     'inline-import-data-uri'
   ],
   presets: ['@babel/preset-env', '@babel/preset-react']
