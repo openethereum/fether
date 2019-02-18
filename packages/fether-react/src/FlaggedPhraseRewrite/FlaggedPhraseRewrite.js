0af964b8658d4da045492102f8eb2f4cc38c2587
YJ <yjkimjunior@gmail.com>
2019-02-18 15:16:46 +0800

    prevent copy paste

--- packages/fether-react/src/FlaggedPhraseRewrite/FlaggedPhraseRewrite.js
+++ packages/fether-react/src/FlaggedPhraseRewrite/FlaggedPhraseRewrite.js
@@ -2,17 +2,57 @@
 // This file is part of Parity.
 //
 // SPDX-License-Identifier: BSD-3-Clause
-
+import { Header, Form as FetherForm } from 'fether-ui';
+import localForage from 'localforage';
 import React, { Component } from 'react';
-import { Header } from 'fether-ui';
 import { Link } from 'react-router-dom';
 
 import RequireHealthOverlay from '../RequireHealthOverlay';
+import withAccount from '../utils/withAccount';
 import Health from '../Health';
 
+@withAccount
 class FlaggedPhraseRewrite extends Component {
+  state = {
+    error: null,
+    phrase: null,
+    value: ''
+  };
+
+  componentDidMount () {
+    this.getPhrase();
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value });
+  };
+
+  getPhrase = async () => {
+    const phrase = await localForage.getItem(
+      `__flagged_${this.props.account.address}`
+    );
+
+    this.setState({ phrase });
+  };
+
+  onCopyOrPastePhrase = event => {
+    // Disable copying the phrase if in production.
+    // Keep it enabled for development.
+    event.preventDefault();
+
+    if (process.env.NODE_ENV !== 'production') {
+      this.setState({
+        error:
+          'Copy and pasting is disabled for this step. Please type out your full recovery phrase.'
+      });
+
+      return false;
+    }
+  };
+
   render () {
     const { history } = this.props;
+    const { error, phrase, value } = this.state;
 
     return (
       <RequireHealthOverlay require='sync'>
@@ -29,8 +69,36 @@ class FlaggedPhraseRewrite extends Component {
             }
             title={<h1>Account Recovery Phrase</h1>}
           />
-          // Show the phrase // Show input to rewrite // Show button to confirm
-          // Unflag account // Redirect to that account
+
+          <div className='text -code' onCopy={this.onCopyOrPastePhrase}>
+            {phrase}
+          </div>
+
+          <FetherForm.Field
+            autoFocus
+            as='textarea'
+            label='Recovery phrase'
+            onChange={this.handleChange}
+            onPaste={this.onCopyOrPastePhrase}
+            required
+            value={value}
+          />
+
+          <nav className='form-nav -space-around'>
+            <button
+              className='button -back'
+              onClick={history.goBack}
+              type='button'
+            >
+              Back
+            </button>
+            <button className='button' disabled={value !== phrase}>
+              Next
+            </button>
+          </nav>
+
+          {error}
+
           <nav className='footer-nav'>
             <div className='footer-nav_status'>
               <Health />
