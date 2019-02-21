d52c4b89383027f166df16805adedd4146600baa
Thibaut <thibaut.sardan@gmail.com>
2019-02-21 14:07:22 +0100

    Revert "test windows"
    This reverts commit 8fec211b59d9e069c5f240cfd30f0841421a8818.

--- packages/fether-electron/src/main/app/methods/setupWinListeners.js
+++ packages/fether-electron/src/main/app/methods/setupWinListeners.js
@@ -58,11 +58,10 @@ function setupWinListeners (fetherApp) {
   win.on('resize', () => {
     pino.info('Detected resize event');
 
-    /*    moveWindowUp(fetherApp);
+    moveWindowUp(fetherApp);
     setTimeout(() => {
       moveWindowUp(fetherApp);
     }, 5000);
-  */
   });
 
   win.on('blur', () => {
