a96768e4e6fa9fd0ab9dad774fbb556bf297bd20
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-12 16:14:57 +0100

    chore: Update copyright headers to 2019 (#462)

--- packages/fether-react/src/Send/TxQrCode/index.js
+++ packages/fether-react/src/Send/TxQrCode/index.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2018 Parity Technologies (UK) Ltd.
+// Copyright 2015-2019 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
 //
 // SPDX-License-Identifier: BSD-3-Clause
