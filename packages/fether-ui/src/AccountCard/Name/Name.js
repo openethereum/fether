62e2c5f5731f9be46e9a72691061351c3f8cdc97
Luke Schoen <ltfschoen@gmail.com>
2019-03-18 18:21:15 +0100

    fix: Use text-overflow ellipsisto concat account name

--- packages/fether-ui/src/AccountCard/Name/Name.js
+++ packages/fether-ui/src/AccountCard/Name/Name.js
@@ -8,16 +8,12 @@ import PropTypes from 'prop-types';
 
 import { Placeholder } from '../../Placeholder';
 
-const MAX_ACCOUNT_NAME_LENGTH = 25;
-
 export const Name = ({ name, screen, ...otherProps }) => (
   <div
     className={`account_name ${screen === 'account' ? '-header' : ''}`}
     {...otherProps}
   >
-    {name.length > MAX_ACCOUNT_NAME_LENGTH
-      ? `${name.substring(0, MAX_ACCOUNT_NAME_LENGTH)}...`
-      : name || <Placeholder height={14} width={100} />}
+    {name || <Placeholder height={14} width={100} />}
   </div>
 );
 
