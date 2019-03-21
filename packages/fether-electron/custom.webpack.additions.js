e510c7b2e2e9fa9cbc0ffa32b0ff7a31b9ece968
Luke Schoen <ltfschoen@gmail.com>
2019-03-21 11:20:00 +0100

    review-fix: Add optional opt-in to using Webpack notifier plugin by running with NOTIFIER=true yarn start

--- packages/fether-electron/custom.webpack.additions.js
+++ packages/fether-electron/custom.webpack.additions.js
@@ -3,16 +3,26 @@
 const path = require('path');
 const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 
-module.exports = {
-  plugins: [
-    new WebpackBuildNotifierPlugin({
-      title: 'Fether Webpack Build',
-      logo: path.resolve('./build/favicon.ico'),
-      suppressSuccess: false,
-      compileIcon: path.resolve('./static/assets/icons/webpack/compile.png'),
-      failureIcon: path.resolve('./static/assets/icons/webpack/failure.png'),
-      successIcon: path.resolve('./static/assets/icons/webpack/success.png'),
-      warningIcon: path.resolve('./static/assets/icons/webpack/warning.png')
-    })
-  ]
-};
+const withWebpackBuildNotifier = process.env.NOTIFIER === 'true';
+
+module.exports = withWebpackBuildNotifier
+  ? {
+    plugins: [
+      new WebpackBuildNotifierPlugin({
+        title: 'Fether Webpack Build',
+        logo: path.resolve('./build/favicon.ico'),
+        suppressSuccess: false,
+        compileIcon: path.resolve(
+          './static/assets/icons/webpack/compile.png'
+        ),
+        failureIcon: path.resolve(
+          './static/assets/icons/webpack/failure.png'
+        ),
+        successIcon: path.resolve(
+          './static/assets/icons/webpack/success.png'
+        ),
+        warningIcon: path.resolve('./static/assets/icons/webpack/warning.png')
+      })
+    ]
+  }
+  : {};
