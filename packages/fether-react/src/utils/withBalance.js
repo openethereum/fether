06a2714b7d68fb72fca0dd56f4505bc37ac1438c
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-22 14:22:47 +0100

    withBalance shows component immeidately

--- packages/fether-react/src/utils/withBalance.js
+++ packages/fether-react/src/utils/withBalance.js
@@ -9,19 +9,25 @@ import branch from 'recompose/branch';
 import compose from 'recompose/compose';
 import { fromWei } from '@parity/api/lib/util/wei';
 import light from '@parity/light.js-react';
-import { map } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 import withProps from 'recompose/withProps';
 
 export const withErc20Balance = light({
   erc20Balance: ({ token, account: { address } }) =>
     makeContract(token.address, abi)
       .balanceOf$(address)
-      .pipe(map(value => value && value.div(10 ** token.decimals)))
+      .pipe(
+        map(value => value && value.div(10 ** token.decimals)),
+        startWith(undefined)
+      )
 });
 
 export const withEthBalance = light({
   ethBalance: ({ account: { address } }) =>
-    balanceOf$(address).pipe(map(value => value && fromWei(value)))
+    balanceOf$(address).pipe(
+      map(value => value && fromWei(value)),
+      startWith(undefined)
+    )
 });
 
 /**
