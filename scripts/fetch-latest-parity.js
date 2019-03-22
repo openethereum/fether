2c7402206609cb228a43e50f4da90353512114d8
Amaury Martiny <amaury.martiny@protonmail.com>
2019-03-22 14:35:44 +0100

    fix: correctly set permissions on downloaded binary

--- scripts/fetch-latest-parity.js
+++ scripts/fetch-latest-parity.js
@@ -125,7 +125,7 @@ function downloadParity () {
           // Write to file and set a+x permissions
           const destinationPath = `${STATIC_DIRECTORY}/${name}`;
           return fsWriteFile(destinationPath, data)
-            .then(() => fsChmod(destinationPath, 755))
+            .then(() => fsChmod(destinationPath, 0o755))
             .then(() => destinationPath);
         });
       })
