aab094d54f1a1f3267fe8980a05c2599ca9069ea
Luke Schoen <ltfschoen@gmail.com>
2019-03-23 06:29:50 +0100

    review-fix: Move WebpackBuildNotifier images so not in binary. Fix ico file

--- packages/fether-electron/custom.webpack.additions.js
+++ packages/fether-electron/custom.webpack.additions.js
@@ -5,23 +5,20 @@ const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 
 const withWebpackBuildNotifier = process.env.NOTIFIER === 'true';
 
+console.log('XXX', path.resolve('./build/favicon.ico'));
+console.log('YYY', path.resolve('./build/icons/webpack/compile.png'));
+
 module.exports = withWebpackBuildNotifier
   ? {
     plugins: [
       new WebpackBuildNotifierPlugin({
         title: 'Fether Webpack Build',
-        logo: path.resolve('./build/favicon.ico'),
+        logo: path.resolve('./build/icons/icon.ico'),
         suppressSuccess: false,
-        compileIcon: path.resolve(
-          './static/assets/icons/webpack/compile.png'
-        ),
-        failureIcon: path.resolve(
-          './static/assets/icons/webpack/failure.png'
-        ),
-        successIcon: path.resolve(
-          './static/assets/icons/webpack/success.png'
-        ),
-        warningIcon: path.resolve('./static/assets/icons/webpack/warning.png')
+        compileIcon: path.resolve('./build/icons/webpack/compile.png'),
+        failureIcon: path.resolve('./build/icons/webpack/failure.png'),
+        successIcon: path.resolve('./build/icons/webpack/success.png'),
+        warningIcon: path.resolve('./build/icons/webpack/warning.png')
       })
     ]
   }
