aeecf99e20277dd0c104fdfca3c4faa6139891ce
Axel Chalon <xaxel@protonmail.com>
2019-04-04 15:27:59 +0200

    refactor: Update craco and remove --react-scripts workaround

--- packages/fether-react/craco.config.js
+++ packages/fether-react/craco.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   babel: {
     plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]]
-  }
+  },
+  // craco isn't compatible with monorepos by default
+  reactScriptsPath: '../../node_modules/react-scripts'
 };
