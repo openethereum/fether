457904596960556da1699f0e481ff1fec87ace27
Luke Schoen <ltfschoen@kitty.berlin.office>
2019-04-05 15:58:49 +0200

    refactor: Refactor tests inside describe blocks

--- packages/fether-electron/src/main/app/utils/isTrustedUrlPattern.spec.js
+++ packages/fether-electron/src/main/app/utils/isTrustedUrlPattern.spec.js
@@ -14,37 +14,41 @@ jest.mock('./pino', () => () => ({
 let url;
 
 describe('trust url pattern', () => {
-  test('should not trust insecure (non-https) links', () => {
-    url = 'http://blockscout.com';
+  describe('blockscout', () => {
+    test('should not trust insecure (non-https) links', () => {
+      url = 'http://blockscout.com';
 
-    expect(isTrustedUrlPattern(url)).toEqual(false);
+      expect(isTrustedUrlPattern(url)).toEqual(false);
+    });
+
+    test('should trust link to internal transaction on ethereum mainnet', () => {
+      url =
+        'https://blockscout.com/eth/mainnet/tx/0xfe7e97d1de24b47d92e815024757e388809425f1681920bc1923368ec1f0fcf1/internal_transactions';
+
+      expect(isTrustedUrlPattern(url)).toEqual(true);
+    });
   });
 
-  test('should trust link to internal transaction on ethereum mainnet', () => {
-    url =
-      'https://blockscout.com/eth/mainnet/tx/0xfe7e97d1de24b47d92e815024757e388809425f1681920bc1923368ec1f0fcf1/internal_transactions';
+  describe('github token icons', () => {
+    test('should trust token .png icons from Github ethcore', () => {
+      url =
+        'https://raw.githubusercontent.com/ethcore/dapp-assets/9e135f76fe9ba61e2d8ccbd72ed144c26c450780/tokens/gavcoin-64x64.png';
 
-    expect(isTrustedUrlPattern(url)).toEqual(true);
-  });
+      expect(isTrustedUrlPattern(url)).toEqual(true);
+    });
 
-  test('should trust token .png icons from Github ethcore', () => {
-    url =
-      'https://raw.githubusercontent.com/ethcore/dapp-assets/9e135f76fe9ba61e2d8ccbd72ed144c26c450780/tokens/gavcoin-64x64.png';
+    test('should trust token .svg icons from Github ethcore', () => {
+      url =
+        'https://raw.githubusercontent.com/ethcore/dapp-assets/9e135f76fe9ba61e2d8ccbd72ed144c26c450780/tokens/gavcoin-64x64.svg';
 
-    expect(isTrustedUrlPattern(url)).toEqual(true);
-  });
+      expect(isTrustedUrlPattern(url)).toEqual(true);
+    });
 
-  test('should trust token .svg icons from Github ethcore', () => {
-    url =
-      'https://raw.githubusercontent.com/ethcore/dapp-assets/9e135f76fe9ba61e2d8ccbd72ed144c26c450780/tokens/gavcoin-64x64.svg';
+    test('should trust token icons from Github atomiclabs', () => {
+      url =
+        'https://raw.githubusercontent.com/atomiclabs/cryptocurrency-icons/tree/master/32/black/arg.png';
 
-    expect(isTrustedUrlPattern(url)).toEqual(true);
-  });
-
-  test('should trust token icons from Github atomiclabs', () => {
-    url =
-      'https://raw.githubusercontent.com/atomiclabs/cryptocurrency-icons/tree/master/32/black/arg.png';
-
-    expect(isTrustedUrlPattern(url)).toEqual(true);
+      expect(isTrustedUrlPattern(url)).toEqual(true);
+    });
   });
 });
