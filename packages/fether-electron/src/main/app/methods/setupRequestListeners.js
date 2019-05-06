6d1d851d2ae927fd3d1d03e97028d68a725c9a1c
Amaury Martiny <amaury.martiny@protonmail.com>
2019-05-06 17:40:44 +0200

    Tighten up CSP

--- packages/fether-electron/src/main/app/methods/setupRequestListeners.js
+++ packages/fether-electron/src/main/app/methods/setupRequestListeners.js
@@ -35,27 +35,6 @@ function setupRequestListeners (fetherApp) {
       callback({ requestHeaders: details.requestHeaders }); // eslint-disable-line
     }
   );
-
-  // Content Security Policy (CSP)
-  // Note: `onHeadersReceived` will not be called in prod, because we use the
-  // file:// protocol: https://electronjs.org/docs/tutorial/security#csp-meta-tag
-  // Instead, the CSP are the ones in the meta tag inside index.html
-  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-    pino.debug(
-      `Configuring Content-Security-Policy for environment ${
-        IS_PROD ? 'production' : 'development'
-      }`
-    );
-
-    /* eslint-disable */
-    callback({
-      responseHeaders: {
-        ...details.responseHeaders,
-        "Content-Security-Policy": [CSP]
-      }
-    });
-    /* eslint-enable */
-  });
 }
 
 export default setupRequestListeners;
