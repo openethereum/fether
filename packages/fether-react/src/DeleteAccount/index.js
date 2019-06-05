7841d5d07a0bb3bf2ab2aa6a27c5b2f317c20788
Amaury Martiny <amaury.martiny@protonmail.com>
2019-06-05 13:29:13 +0200

    Add correct screens

---
+++ packages/fether-react/src/DeleteAccount/index.js
@@ -0,0 +1,8 @@
+// Copyright 2015-2019 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+import DeleteAccount from './DeleteAccount';
+
+export default DeleteAccount;
