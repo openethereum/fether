1addb0e43f7049edf8ffe9aa26a1680d9338a313
thib <thibaut.sardan@gmail.com>
2019-08-30 16:58:50 +0200

    chore: use latest commit

--- packages/fether-electron/src/main/app/utils/isTrustedUrlPattern.spec.js
+++ packages/fether-electron/src/main/app/utils/isTrustedUrlPattern.spec.js
@@ -46,7 +46,7 @@ describe('trust url pattern', () => {
 
     test('should trust token icons from Github atomiclabs', () => {
       url =
-        'https://raw.githubusercontent.com/atomiclabs/cryptocurrency-icons/25376220e0a5cda085b6b25a7a6d528531246b89/32/black/arg.png';
+        'https://raw.githubusercontent.com/atomiclabs/cryptocurrency-icons/9867bdb19da14e63ffbe63805298fa60bf255cdd/32/black/arg.png';
 
       expect(isTrustedUrlPattern(url)).toBe(true);
     });
