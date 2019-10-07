b00b55242e518ac89e45661bf8b071c77d7e9dbd
Axel Chalon <xaxel@protonmail.com>
2019-10-07 16:00:07 +0200

    Fix tests

--- packages/fether-electron/src/main/app/utils/pino.js
+++ packages/fether-electron/src/main/app/utils/pino.js
@@ -14,22 +14,27 @@ import { name } from '../../../../package.json';
 const pretty = Pino.pretty();
 pretty.pipe(process.stdout);
 
-// Create userData folder if it doesn't exist
-try {
-  fs.statSync(app.getPath('userData'));
-} catch (e) {
-  fs.mkdirSync(app.getPath('userData'));
-}
+const streams = [];
 
-// Create 2 output streams:
+// In production, create 2 output streams:
 // - fether.log file (raw JSON)
 // - stdout (prettified output)
-const streams = [
-  {
+const IS_TEST = !app;
+
+if (!IS_TEST) {
+  // Create userData folder if it doesn't exist
+  try {
+    fs.statSync(app.getPath('userData'));
+  } catch (e) {
+    fs.mkdirSync(app.getPath('userData'));
+  }
+
+  streams.push({
     level: 'info',
     stream: fs.createWriteStream(`${app.getPath('userData')}/${name}.log`)
-  },
-  { level: 'info', stream: pretty }
-];
+  });
+}
+
+streams.push({ level: 'info', stream: pretty });
 
 export default opts => Pino({ name, ...opts }, multistream(streams));
