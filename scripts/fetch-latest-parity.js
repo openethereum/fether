9f0251e0a05f4c74dea25e931884ea64ab6023ff
thib <thibaut.sardan@gmail.com>
2020-02-07 07:22:33 +0100

    bump

--- scripts/fetch-latest-parity.js
+++ scripts/fetch-latest-parity.js
@@ -53,6 +53,7 @@ const foundPath = [
   path.join(STATIC_DIRECTORY, 'parity.exe')
 ].find(existsSync);
 
+console.log('-----------> found pat', foundPath);
 if (foundPath) {
   // Bundled Parity was found, we check if the version matches the minimum requirements
   getBinaryVersion(foundPath)
